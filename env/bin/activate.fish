5555f8ec4445ad69a4e84e08552cc667197aca08
Ahmed A. Olawale <ayobamyahmed@gmail.com>
2022-10-16 21:25:48 +0000

    [feat]: enviroment

--- env/bin/activate.fish
+++ env/bin/activate.fish
@@ -29,7 +29,7 @@ end
 # unset irrelevant variables
 deactivate nondestructive
 
-set -gx VIRTUAL_ENV "/workspace/Backend-Zuri/songcrud/env"
+set -gx VIRTUAL_ENV "/workspace/Backend-Zuri/env"
 
 set -gx _OLD_VIRTUAL_PATH $PATH
 set -gx PATH "$VIRTUAL_ENV/bin" $PATH
